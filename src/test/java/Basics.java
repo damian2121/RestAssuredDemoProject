db6c43a4d2f53f707b5eaac019604be342a98646
Damian Kogut <damian.kogut1997@gmail.com>
2025-07-26 14:56:43 +0200

    created new payload class that stores request body payloads. Also added capturing response into string and extracting property value and storing it into variable.

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -1,5 +1,9 @@
+import files.payload;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class Basics {
 
@@ -7,32 +11,20 @@ public class Basics {
 
         //validate if Add Place API is working as expected
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given()
+        String response = given()
             .log().all()
             .queryParam("key", "qaclick123")
             .header("Content-Type", "application/json")
-            .body("""
-                    {
-                      "location": {
-                        "lat": -38.383494,
-                        "lng": 33.427362
-                      },
-                      "accuracy": 50,
-                      "name": "Frontline house",
-                      "phone_number": "(+91) 983 893 3937",
-                      "address": "29, side layout, cohen 09",
-                      "types": [
-                        "shoe park",
-                        "shop"
-                      ],
-                      "website": "http://google.com",
-                      "language": "French-IN"
-                    }
-                    """)
+            .body(payload.AddPlace())
         .when()
             .post("/maps/api/place/add/json")
         .then()
-                .log().all().assertThat().statusCode(200);
+                .log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
+                .header("Server", "Apache/2.4.52 (Ubuntu)")
+                .extract().response().asString();
 
+        JsonPath js = new JsonPath(response);
+        String placeId = js.getString("place_id");
+        
     }
 }
